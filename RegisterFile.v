cc16eebb962a09c5e52681755c77f0a124c00c61
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-16 23:49:58 +0200

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,10 +1,11 @@
-module registerFile(ReadData1, ReadData2, clk, ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite);
+module regFile(reg1, reg2, reg3, ReadData1, ReadData2, clk, ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite);
 //input
 	input [4:0] ReadRegister1, ReadRegister2, WriteRegister;
 	input clk, RegWrite;
 	input [31:0] WriteData;
 //output	
 	output reg [31:0] ReadData1, ReadData2;
+	output reg [31:0]reg1,reg2,reg3;
 	
 //initializing registers
 	reg [31:0] registers_i[31:0];	
@@ -17,7 +18,6 @@ module registerFile(ReadData1, ReadData2, clk, ReadRegister1, ReadRegister2, Wri
 				begin
 					registers_i[i]=i;
 				end
-				
 		end
 //writing data to the register
 			always @(posedge clk)
@@ -25,6 +25,8 @@ module registerFile(ReadData1, ReadData2, clk, ReadRegister1, ReadRegister2, Wri
 					if(RegWrite && WriteRegister!=0)
 						begin
 							registers_i[WriteRegister] <= WriteData;
+							//here
+							reg3 <= registers_i[6];
 						end
 				end
 						
@@ -33,54 +35,57 @@ module registerFile(ReadData1, ReadData2, clk, ReadRegister1, ReadRegister2, Wri
 		begin
 			ReadData1 <= registers_i[ReadRegister1];
 			ReadData2 <= registers_i[ReadRegister2];
+			reg1 <= registers_i[1];
+         reg2 <= registers_i[2];
+	      
 		end
-			
+	
 endmodule
-//////////////////////////////////////////////////
-//module clock(clk);
-//	output clk;
-//	reg clk;
-//	initial begin
-//		clk <= 0;
-//	end
-//	always
-//		begin
-//		#50 
-//		clk <= ~clk;	
-//	end
-//endmodule
 ////////////////////////////////////////////////////
-module test;
-wire [31:0] r1, r2;
-reg [31:0] wd;
-reg [4:0] rr1, rr2, w;
-wire clk;
-reg rw;
-
-clock c1(clk);
-registerFile rf(r1,r2,clk,rr1,rr2,w,wd,rw);
-
-initial
-begin
-rr1 = 2 ; rr2 = 3 ; w=0 ; wd = 0; rw =0;
-#50 $display("R1= %b ",r1," R2 = %b",r2);
-
-rr1 = 2 ; rr2 = 3 ; w=4 ; wd = 16; rw =1;
-#75 $display("R1= %b ",r1," R2 = %b",r2);
-
-rr1 = 4 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
-#75 $display("R1= %b ",r1," R2 = %b",r2);
-
-rr1 = 4 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
-#76 $display("R1= %b ",r1," R2 = %b",r2);
-
-rr1 = 0 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
-#100 $display("R1= %b ",r1," R2 = %b",r2);
-
-rr1 = 2 ; rr2 = 3 ; w=0 ; wd = 0; rw =0;
-#150 $display("R1= %b ",r1," R2 = %b",r2);
-
-end
-endmodule
-
-
+////module clock(clk);
+////	output clk;
+////	reg clk;
+////	initial begin
+////		clk <= 0;
+////	end
+////	always
+////		begin
+////		#50 
+////		clk <= ~clk;	
+////	end
+////endmodule
+//////////////////////////////////////////////////////
+////module test;
+////wire [31:0] r1, r2;
+////reg [31:0] wd;
+////reg [4:0] rr1, rr2, w;
+////wire clk;
+////reg rw;
+////
+////clock c1(clk);
+////registerFile rf(r1,r2,clk,rr1,rr2,w,wd,rw);
+////
+////initial
+////begin
+////rr1 = 2 ; rr2 = 3 ; w=0 ; wd = 0; rw =0;
+////#50 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////rr1 = 2 ; rr2 = 3 ; w=4 ; wd = 16; rw =1;
+////#75 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////rr1 = 4 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
+////#75 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////rr1 = 4 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
+////#76 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////rr1 = 0 ; rr2 = 4 ; w= 0; wd = 16; rw =1;
+////#100 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////rr1 = 2 ; rr2 = 3 ; w=0 ; wd = 0; rw =0;
+////#150 $display("R1= %b ",r1," R2 = %b",r2);
+////
+////end
+////endmodule
+////
+////
