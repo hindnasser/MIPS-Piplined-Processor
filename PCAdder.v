32c020aeef0845c6b093d58eeda5e0e300c7ce02
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:20:27 +0200

    Update PCAdder.v

--- PCAdder.v
+++ PCAdder.v
@@ -1,3 +1,15 @@
-module PCAdder ();
+module PCAdder (PCplus4, PC_value, four);
 
+// input 
+	input [31:0] PC_value;
+	input [2:0] four;
+	
+// output
+	output [31:0] PCplus4;
+	
+	assign PCplus4 = PC_value + 4;
+	
+	
 endmodule
+
+
