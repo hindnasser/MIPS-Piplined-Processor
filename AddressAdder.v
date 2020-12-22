e60e455c0b1d8e28e1d2e57412f9a46f8a40f121
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:12:34 +0200

    Update AddressAdder.v

--- AddressAdder.v
+++ AddressAdder.v
@@ -1,3 +1,12 @@
-module AddressAdder ();
+module AddressAdder (EXE_BranchAddress, PCplus4, BranchAdd);
+
+// input
+	input [31:0] PCplus4, BranchAdd;
+
+// output
+	output [31:0] EXE_BranchAddress;
+	
+	assign EXE_BranchAddress = PCplus4 + BranchAdd;
+
 
 endmodule
