9cb45f3be3bdbd036fc4d5829fbb85363eacc955
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:15:06 +0200

    Update EXE_MEM_Register.v

--- EXE_MEM_Register.v
+++ EXE_MEM_Register.v
@@ -1,2 +1,29 @@
-module EXE_MEM_Register ();
+module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, EXE_MEM_Rd, EXE_MEM_Zero, BranchEqualOut, BranchnotEqualOut, MemReadOut,
+		 MemWriteOut, MemtoRegOut, RegWriteOut, EXE_BranchAddress, EXE_Result, EXE_DstReg, ID_EXE_Rd, EXE_Zero, BranchEqualIn, BranchnotEqualIn, 
+		 MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk);
+
+// input
+	input [31:0] EXE_BranchAddress, EXE_Result;
+	input [4:0] EXE_DstReg, ID_EXE_Rd;
+	input EXE_Zero, BranchEqualIn, BranchnotEqualIn, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk;
+	
+// output
+	output reg [31:0] EXE_MEM_Result, EXE_MEM_BranchAddress;
+	output reg [4:0] EXE_MEM_DstReg, EXE_MEM_Rd;
+	output reg EXE_MEM_Zero, BranchEqualOut, BranchnotEqualOut, MemReadOut, MemWriteOut, MemtoRegOut, RegWriteOut;
+	
+	always @(posedge clk)
+		begin
+			EXE_MEM_Result = EXE_Result;
+			EXE_MEM_BranchAddress = EXE_BranchAddress;
+			EXE_MEM_DstReg = EXE_DstReg;
+			EXE_MEM_Rd = ID_EXE_Rd;
+			EXE_MEM_Zero = EXE_Zero;
+			BranchEqualOut = BranchEqualIn;
+			BranchnotEqualOut = BranchnotEqualIn;
+			MemReadOut = MemReadIn;
+			MemWriteOut = MemWriteIn;
+			MemtoRegOut = MemtoRegIn;
+			RegWriteOut = RegWriteIn;
+		end
 endmodule
