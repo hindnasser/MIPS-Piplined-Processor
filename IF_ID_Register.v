d9e45975a5eda0fddb8919b84f6ae4da202faccd
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:18:28 +0200

    Update IF_ID_Register.v

--- IF_ID_Register.v
+++ IF_ID_Register.v
@@ -1,26 +1,27 @@
-module IF_ID_Register(instructionOut, PCplus4Out, instructionIn, IF_Flush, clk, PCplus4);
+module IF_ID_Register(IF_ID_Rs, IF_ID_Rt, IF_ID_Rd, IF_ID_Opcode, IF_ID_Shamt, IF_ID_Func, IF_ID_Immediate,
+	    IF_ID_Address, IF_ID_PCplus4Out, instructionIn, clk, PCplus4);
 //input
 	input [31:0] instructionIn, PCplus4;
-	input IF_Flush, clk;
+	input clk;
 		
 //output
-	output reg [31:0] instructionOut;
-	output reg [31:0] PCplus4Out;
+	output reg [4:0] IF_ID_Rs, IF_ID_Rt, IF_ID_Rd, IF_ID_Shamt;
+	output reg [5:0] IF_ID_Func, IF_ID_Opcode;
+	output reg [15:0] IF_ID_Immediate;
+	output reg [25:0] IF_ID_Address;
+	output reg [31:0] IF_ID_PCplus4Out;
 	
 	always @(posedge clk) 
 		begin
-//To fetch a nop
-			if(IF_Flush == 1)
-				begin
-					instructionOut = 32'b0 ;
-					PCplus4Out = PCplus4;
-				end
-//To fetch the next instruction		
-			else 
-				begin
-					instructionOut = instructionIn;
-					PCplus4Out = PCplus4;
-				end
+			IF_ID_Rs = instructionIn[25:21];
+			IF_ID_Rt = instructionIn [20:16];
+			IF_ID_Rd = instructionIn [15:11];
+			IF_ID_Shamt = instructionIn [10:6];
+			IF_ID_Func = instructionIn [5:0];
+			IF_ID_Opcode = instructionIn [31:26];
+			IF_ID_Immediate = instructionIn [15:0];
+			IF_ID_Address = instructionIn [25:0];
+			IF_ID_PCplus4Out = PCplus4;
 		end
 	
 endmodule
