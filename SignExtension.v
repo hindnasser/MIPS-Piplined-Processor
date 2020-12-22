6a4fb1e86b88bd2c747c3dc25b6087fe5fbb8111
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:23:11 +0200

    Update SignExtension.v

--- SignExtension.v
+++ SignExtension.v
@@ -1,3 +1,19 @@
-module SignExtension ();
+module SignExtension (SignedImmediate, immediate);
 
+// input 
+	input [15:0] immediate;
+
+// output 
+	output reg [31:0] SignedImmediate;
+	
+	always @(*) begin
+		SignedImmediate [15:0] = immediate [15:0];
+		
+		if(immediate[15]==1)
+			SignedImmediate [31:16] = 16'hFFFF;
+		else 
+			SignedImmediate [31:16] = 16'h0000;
+	end
 endmodule
+
+
