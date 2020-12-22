0c3676a95e35d966704ec262db691e0a5f47070f
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-22 16:23:51 +0200

    Update ZeroExtension.v

--- ZeroExtension.v
+++ ZeroExtension.v
@@ -1,3 +1,12 @@
-module ZeroExtension ();
+module ZeroExtension (UnsignedImmediate, Immediate);
+
+// input
+	input [15:0] Immediate;
+	
+// output
+	output [31:0] UnsignedImmediate;
+	
+	assign UnsignedImmediate [15:0] = Immediate [15:0];
+	assign UnsignedImmediate [31:16] = 16'h0000;
  
 endmodule
