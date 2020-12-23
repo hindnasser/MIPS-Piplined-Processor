8ae8853e3d99e66e47c68b9698ea9371ce0fcf0a
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-23 21:42:15 +0200

    Add files via upload

---
+++ AddressOr.v
@@ -0,0 +1,11 @@
+module AddressOr (PC_Src, BranchEqualResult, BranchnotEqualResult);
+
+// input 
+	input BranchEqualResult, BranchnotEqualResult;
+	
+// output
+	output PC_Src;
+	
+	assign PC_Src = BranchEqualResult | BranchnotEqualResult;
+	
+endmodule
