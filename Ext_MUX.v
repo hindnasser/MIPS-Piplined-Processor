e68dbbeb33d3d91298d685de660cff71dcd57865
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-23 21:55:51 +0200

    Create Ext_MUX.v

---
+++ Ext_MUX.v
@@ -0,0 +1,19 @@
+module Ext_MUX (ExtendedImm, SignedImmediate, UnsignedImmediate, Issigned, clk)
+
+// input 
+	input [31:0] SignedImmediate, UnsignedImmediate;
+	input Issigned, clk;
+	
+// output
+	output [31:0] ExtendedImm;
+	
+	always @(posedge clk)
+		begin
+			if(Issigned == 0)
+				ExtendedImm = UnsignedImmediate;
+			else if(Issigned == 1)
+				ExtendedImm = SignedImmediate;
+		end
+endmodule
+
+		
