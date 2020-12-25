4e6dfc904718fa08a19a70430427cbcb791f9562
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-25 13:44:39 +0200

    Update MEM_WB_Register.v

--- MEM_WB_Register.v
+++ MEM_WB_Register.v
@@ -1,5 +1,5 @@
-module MEM_WB_Register (MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_Rd, MEM_WB_DstReg, MemtoRegOut, RegWriteOut, MEM_Result, EXE_MEM_Result,
-		 EXE_MEM_Rd, EXE_MEM_DstReg, MemtoRegIn, RegWriteIn, clk);
+module MEM_WB_Register (MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_Rd, MEM_WB_DstReg, MEM_WB_MemtoReg, MEM_WB_RegWrite, MEM_Result, EXE_MEM_Result,
+								EXE_MEM_Rd, EXE_MEM_DstReg, MemtoRegIn, RegWriteIn, clk);
 
 // input
 	input [31:0] MEM_Result, EXE_MEM_Result;
@@ -9,7 +9,7 @@ module MEM_WB_Register (MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_Rd, MEM_WB_DstReg
 // output
 	output reg [31:0] MEM_WB_MemData, MEM_WB_ALUData;
 	output reg [4:0] MEM_WB_Rd, MEM_WB_DstReg;
-	output reg MemtoRegOut, RegWriteOut;
+	output reg MEM_WB_MemtoReg, MEM_WB_RegWrite;
 	
 	always @(negedge clk)
 		begin
@@ -17,8 +17,8 @@ module MEM_WB_Register (MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_Rd, MEM_WB_DstReg
 			MEM_WB_ALUData = EXE_MEM_Result;
 			MEM_WB_Rd = EXE_MEM_Rd;
 			MEM_WB_DstReg = EXE_MEM_DstReg;
-			MemtoRegOut = MemtoRegIn;
-			RegWriteOut = RegWriteIn;
+			MEM_WB_MemtoReg = MemtoRegIn;
+			MEM_WB_RegWrite = RegWriteIn;
 		end
 
 endmodule
