1828c3e60daae2fe854bb792ae0d3998be17598c
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-25 13:40:10 +0200

    Update EXE_MEM_Register.v

--- EXE_MEM_Register.v
+++ EXE_MEM_Register.v
@@ -1,6 +1,6 @@
-module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, EXE_MEM_Rd, EXE_MEM_Zero, EXE_MEM_Rt, BranchEqualOut, BranchnotEqualOut, MemReadOut,
-		 MemWriteOut, MemtoRegOut, RegWriteOut, EXE_BranchAddress, EXE_Result, EXE_DstReg, ID_EXE_Rd, ID_EXE_Rt, EXE_Zero, BranchEqualIn, BranchnotEqualIn, 
-		 MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk);
+module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, EXE_MEM_Rd, EXE_MEM_Zero, EXE_MEM_Rt, EXE_MEM_BranchEqual, EXE_MEM_BranchnotEqual,
+								 EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite, EXE_BranchAddress, EXE_Result, EXE_DstReg, ID_EXE_Rd, ID_EXE_Rt,
+								 EXE_Zero, BranchEqualIn, BranchnotEqualIn, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk);
 
 // input
 	input [31:0] EXE_BranchAddress, EXE_Result, ID_EXE_Rt;
@@ -10,7 +10,7 @@ module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, 
 // output
 	output reg [31:0] EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_Rt;
 	output reg [4:0] EXE_MEM_DstReg, EXE_MEM_Rd;
-	output reg EXE_MEM_Zero, BranchEqualOut, BranchnotEqualOut, MemReadOut, MemWriteOut, MemtoRegOut, RegWriteOut;
+	output reg EXE_MEM_Zero, EXE_MEM_BranchEqual, EXE_MEM_BranchnotEqual, EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite;
 	
 	always @(posedge clk)
 		begin
@@ -19,12 +19,12 @@ module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, 
 			EXE_MEM_DstReg = EXE_DstReg;
 			EXE_MEM_Rd = ID_EXE_Rd;
 			EXE_MEM_Zero = EXE_Zero;
-			BranchEqualOut = BranchEqualIn;
-			BranchnotEqualOut = BranchnotEqualIn;
-			MemReadOut = MemReadIn;
-			MemWriteOut = MemWriteIn;
-			MemtoRegOut = MemtoRegIn;
-			RegWriteOut = RegWriteIn;
+			EXE_MEM_BranchEqual = BranchEqualIn;
+			EXE_MEM_BranchnotEqual = BranchnotEqualIn;
+			EXE_MEM_MemRead = MemReadIn;
+			EXE_MEM_MemWrite = MemWriteIn;
+			EXE_MEM_MemtoReg = MemtoRegIn;
+			EXE_MEM_RegWrite = RegWriteIn;
 			EXE_MEM_Rt = ID_EXE_Rt;			
 		end
 endmodule
