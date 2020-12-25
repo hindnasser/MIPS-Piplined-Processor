619b8b9c911c15460a0d31fe459b4e4a6fffb231
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-25 13:43:52 +0200

    Update ID_EXE_Register.v

--- ID_EXE_Register.v
+++ ID_EXE_Register.v
@@ -1,34 +1,35 @@
-module ID_EXE_Register (ID_EXE_Func, ID_EXE_PCplus4, ID_EXE_Rs, ID_EXE_Rt, ID_EXE_Rd, ID_EXE_ExtendedImm, ID_EXE_Shamt, RegDstOut, RegWriteOut, MemtoRegOut, 
-                        JmpandLinkOut, MemReadOut, MemWriteOut, BranchEqualOut, BranchnotEqualOut, ALUopOut, ALUSrcOut, IF_ID_Shamt, IF_ID_Func, IF_ID_PCplus4, 
-								ID_Rs, ID_Rt, IF_ID_Rd, ExtendedImm, RegDstIn, RegWriteIn, MemtoRegIn, JmpandLinkIn, MemReadIn, 
-								MemWriteIn, BranchEqualIn, BranchnotEqualIn, ALUopIn, ALUSrcIn, clk);
+module ID_EXE_Register (ID_EXE_Func, ID_EXE_PCplus4, ID_EXE_Rs, ID_EXE_Rt, ID_EXE_Rd, ID_EXE_ExtendedImm, ID_EXE_Shamt, ID_EXE_RegDst, ID_EXE_RegWrite, 
+                        ID_EXE_MemtoReg, ID_EXE_JmpandLink, ID_EXE_MemRead, ID_EXE_MemWrite, ID_EXE_BranchEqual, ID_EXE_BranchnotEqual, ID_EXE_ALUop,
+								ID_EXE_ALUSrc, IF_ID_Shamt, IF_ID_Func, IF_ID_PCplus4, ID_Rs, ID_Rt, IF_ID_Rd, ExtendedImm, RegDstIn, RegWriteIn, MemtoRegIn, 
+								JmpandLinkIn, MemReadIn, MemWriteIn, BranchEqualIn, BranchnotEqualIn, ALUopIn, ALUSrcIn, clk);
 //input
 	input RegDstIn, RegWriteIn, MemtoRegIn, JmpandLinkIn, MemReadIn, MemWriteIn, BranchEqualIn, BranchnotEqualIn, ALUSrcIn, clk;
 	input [3:0] ALUopIn;
-	input [31:0] ID_Rs, ID_Rt,IF_ID_Rd, ExtendedImm, IF_ID_PCplus4;
+	input [31:0] ID_Rs, ID_Rt, ExtendedImm, IF_ID_PCplus4;
 	input [5:0] IF_ID_Func;
-	input [4:0] IF_ID_Shamt;
+	input [4:0] IF_ID_Shamt, IF_ID_Rd;
 	
 //output
-	output reg RegDstOut, RegWriteOut, MemtoRegOut, JmpandLinkOut, MemReadOut, MemWriteOut, BranchEqualOut, BranchnotEqualOut, ALUSrcOut;
-	output reg [3:0] ALUopOut;
-	output reg [31:0] ID_EXE_PCplus4, ID_EXE_Rs, ID_EXE_Rt, ID_EXE_Rd, ID_EXE_ExtendedImm;
+	output reg ID_EXE_RegDst, ID_EXE_RegWrite, ID_EXE_MemtoReg, ID_EXE_JmpandLink, ID_EXE_MemRead, ID_EXE_MemWrite, ID_EXE_BranchEqual, ID_EXE_BranchnotEqual,
+              ID_EXE_ALUSrc;
+	output reg [3:0] ID_EXE_ALUop;
+	output reg [31:0] ID_EXE_PCplus4, ID_EXE_Rs, ID_EXE_Rt, ID_EXE_ExtendedImm;
 	output reg [5:0] ID_EXE_Func;
-	output reg [4:0] ID_EXE_Shamt;
+	output reg [4:0] ID_EXE_Shamt, ID_EXE_Rd;
 	
 	
 	always @(posedge clk) 
 		begin
-			RegDstOut = RegDstIn;
-			RegWriteOut = RegWriteIn;
-			MemtoRegOut = MemtoRegIn;
-			JmpandLinkOut = JmpandLinkIn;
-			MemReadOut = MemReadIn;
-			MemWriteOut = MemWriteIn;
-			BranchEqualOut = BranchEqualIn;
-			BranchnotEqualOut = BranchEqualIn;
-			ALUSrcOut = ALUSrcIn;
-			ALUopOut = ALUopIn;
+			ID_EXE_RegDst = RegDstIn;
+			ID_EXE_RegWrite = RegWriteIn;
+			ID_EXE_MemtoReg = MemtoRegIn;
+			ID_EXE_JmpandLink = JmpandLinkIn;
+			ID_EXE_MemRead = MemReadIn;
+			ID_EXE_MemWrite = MemWriteIn;
+			ID_EXE_BranchEqual = BranchEqualIn;
+			ID_EXE_BranchnotEqual = BranchEqualIn;
+			ID_EXE_ALUSrc = ALUSrcIn;
+			ID_EXE_ALUop = ALUopIn;
 			ID_EXE_PCplus4 = IF_ID_PCplus4;
 			ID_EXE_Rs = ID_Rs;
 			ID_EXE_Rt = ID_Rt;
