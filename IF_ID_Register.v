a73c4fe1b2501fb11cdf9bd6ed127d8bb289d993
hindnasser <48987233+hindnasser@users.noreply.github.com>
2021-01-04 16:31:50 +0200

    Update IF_ID_Register.v

--- IF_ID_Register.v
+++ IF_ID_Register.v
@@ -1,8 +1,8 @@
 module IF_ID_Register(IF_ID_Rs, IF_ID_Rt, IF_ID_Rd, IF_ID_Opcode, IF_ID_Shamt, IF_ID_Func, IF_ID_Immediate,
-							 IF_ID_Address, IF_ID_PCplus4, instruction, PCplus4, clk);
+							 IF_ID_Address, IF_ID_PCplus4, instruction, PCplus4, clk, IF_ID_Write);
 //input
 	input [31:0] instruction, PCplus4;
-	input clk;
+	input clk, IF_ID_Write;
 		
 //output
 	output reg [4:0] IF_ID_Rs, IF_ID_Rt, IF_ID_Rd, IF_ID_Shamt;
@@ -11,17 +11,19 @@ module IF_ID_Register(IF_ID_Rs, IF_ID_Rt, IF_ID_Rd, IF_ID_Opcode, IF_ID_Shamt, I
 	output reg [25:0] IF_ID_Address;
 	output reg [31:0] IF_ID_PCplus4;
 	
-	always @(posedge clk) 
+	always @(posedge clk)
 		begin
-			IF_ID_Rs <= instruction[25:21];
-			IF_ID_Rt <= instruction [20:16];
-			IF_ID_Rd <= instruction [15:11];
-			IF_ID_Shamt <= instruction [10:6];
-			IF_ID_Func <= instruction [5:0];
-			IF_ID_Opcode <= instruction [31:26];
-			IF_ID_Immediate <= instruction [15:0];
-			IF_ID_Address <= instruction [25:0];
-			IF_ID_PCplus4 <= PCplus4;
+			if(IF_ID_Write == 1)
+				begin
+					IF_ID_Rs <= instruction[25:21];
+					IF_ID_Rt <= instruction [20:16];
+					IF_ID_Rd <= instruction [15:11];
+					IF_ID_Shamt <= instruction [10:6];
+					IF_ID_Func <= instruction [5:0];
+					IF_ID_Opcode <= instruction [31:26];
+					IF_ID_Immediate <= instruction [15:0];
+					IF_ID_Address <= instruction [25:0];
+					IF_ID_PCplus4 <= PCplus4;
+				end
 		end
-	
 endmodule
