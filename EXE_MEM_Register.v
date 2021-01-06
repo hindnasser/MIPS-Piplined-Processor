591f6ed6a0716de2adbab6c6296d005b1431101b
hindnasser <48987233+hindnasser@users.noreply.github.com>
2021-01-06 16:45:32 +0200

    Update EXE_MEM_Register.v

--- EXE_MEM_Register.v
+++ EXE_MEM_Register.v
@@ -1,25 +1,21 @@
-module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_DstReg, EXE_MEM_Zero, EXE_MEM_Rt, EXE_MEM_BranchEqual, EXE_MEM_BranchnotEqual,
-								 EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite, EXE_BranchAddress, EXE_Result, EXE_DstReg, ID_EXE_Rt,
-								 EXE_Zero, BranchEqualIn, BranchnotEqualIn, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk);
+module EXE_MEM_Register (EXE_MEM_Result, EXE_MEM_DstReg, EXE_MEM_Rt,EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite,
+								 EXE_Result, EXE_DstReg, ID_EXE_Rt, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk);
 
 // input
-	input [31:0] EXE_BranchAddress, EXE_Result, ID_EXE_Rt;
+	input [31:0] EXE_Result, ID_EXE_Rt;
 	input [4:0] EXE_DstReg;
-	input EXE_Zero, BranchEqualIn, BranchnotEqualIn, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk;
+	input MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, clk;
+//	clk;
 	
 // output
-	output reg [31:0] EXE_MEM_Result, EXE_MEM_BranchAddress, EXE_MEM_Rt;
+	output reg [31:0] EXE_MEM_Result, EXE_MEM_Rt;
 	output reg [4:0] EXE_MEM_DstReg;
-	output reg EXE_MEM_Zero, EXE_MEM_BranchEqual, EXE_MEM_BranchnotEqual, EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite;
+	output reg EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite;
 	
 	always @(posedge clk)
 		begin
 			EXE_MEM_Result <= EXE_Result;
-			EXE_MEM_BranchAddress <= EXE_BranchAddress;
 			EXE_MEM_DstReg <= EXE_DstReg;
-			EXE_MEM_Zero <= EXE_Zero;
-			EXE_MEM_BranchEqual <= BranchEqualIn;
-			EXE_MEM_BranchnotEqual <= BranchnotEqualIn;
 			EXE_MEM_MemRead <= MemReadIn;
 			EXE_MEM_MemWrite <= MemWriteIn;
 			EXE_MEM_MemtoReg <= MemtoRegIn;
