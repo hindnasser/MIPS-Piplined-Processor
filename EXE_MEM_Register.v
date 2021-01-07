0280db63afa3fa8c4a13f512b63004b780519627
hindnasser <48987233+hindnasser@users.noreply.github.com>
2021-01-07 22:38:31 +0200

    Update EXE_MEM_Register.v

--- EXE_MEM_Register.v
+++ EXE_MEM_Register.v
@@ -1,16 +1,16 @@
 module EXE_MEM_Register (EXE_MEM_R_memtoReg, EXE_MEM_ReadfromMem, EXE_MEM_WritetoMem, EXE_MEM_Result, EXE_MEM_DstReg, EXE_MEM_Rt,EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite,
-								 EXE_Result, EXE_DstReg, ID_EXE_Rt, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, EXE_ReadfromMem, EXE_WritetoMem, EXE_R_memtoReg, clk);
+								 EXE_MEM_Byte, ID_EXE_Byte, EXE_Result, EXE_DstReg, ID_EXE_Rt, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, EXE_ReadfromMem, EXE_WritetoMem, EXE_R_memtoReg, clk);
 
 // input
 	input [31:0] EXE_Result, ID_EXE_Rt;
 	input [4:0] EXE_DstReg;
-	input MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, EXE_ReadfromMem, EXE_WritetoMem, EXE_R_memtoReg, clk;
+	input ID_EXE_Byte, MemReadIn, MemWriteIn, MemtoRegIn, RegWriteIn, EXE_ReadfromMem, EXE_WritetoMem, EXE_R_memtoReg, clk;
 //	clk;
 	
 // output
 	output reg [31:0] EXE_MEM_Result, EXE_MEM_Rt;
 	output reg [4:0] EXE_MEM_DstReg;
-	output reg EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite, EXE_MEM_ReadfromMem, EXE_MEM_WritetoMem, EXE_MEM_R_memtoReg;
+	output reg EXE_MEM_Byte, EXE_MEM_MemRead, EXE_MEM_MemWrite, EXE_MEM_MemtoReg, EXE_MEM_RegWrite, EXE_MEM_ReadfromMem, EXE_MEM_WritetoMem, EXE_MEM_R_memtoReg;
 	
 	always @(posedge clk)
 		begin
@@ -24,6 +24,7 @@ module EXE_MEM_Register (EXE_MEM_R_memtoReg, EXE_MEM_ReadfromMem, EXE_MEM_Writet
 			EXE_MEM_ReadfromMem <= EXE_ReadfromMem;
 			EXE_MEM_WritetoMem <= EXE_WritetoMem;
 			EXE_MEM_R_memtoReg <= EXE_R_memtoReg;
+			EXE_MEM_Byte <= ID_EXE_Byte;
 				
 		end
 endmodule
