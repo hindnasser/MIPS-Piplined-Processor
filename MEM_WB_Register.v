4c80e6c5d6856d75c001dd7d4ad8040e7f5eedc3
hindnasser <48987233+hindnasser@users.noreply.github.com>
2021-01-07 23:45:33 +0200

    Update MEM_WB_Register.v

--- MEM_WB_Register.v
+++ MEM_WB_Register.v
@@ -1,15 +1,15 @@
-module MEM_WB_Register (MEM_WB_R_memtoReg, MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_DstReg, MEM_WB_MemtoReg, MEM_WB_RegWrite, MEM_Result, EXE_MEM_Result,
+module MEM_WB_Register (MEM_WB_R_memtoReg, MEM_WB_MemData, MEM_WB_ALUData, MEM_WB_DstReg, MEM_WB_MemtoReg, MEM_WB_RegWrite, MEM_WB_JmpandLink, EXE_MEM_JmpandLink, MEM_Result, EXE_MEM_Result,
 							   EXE_MEM_DstReg, MemtoRegIn, RegWriteIn, MEM_R_memtoReg, clk);
 
 // input
 	input [31:0] MEM_Result, EXE_MEM_Result;
 	input [4:0] EXE_MEM_DstReg;
-	input MemtoRegIn, RegWriteIn, MEM_R_memtoReg, clk;
+	input EXE_MEM_JmpandLink, MemtoRegIn, RegWriteIn, MEM_R_memtoReg, clk;
 
 // output
 	output reg [31:0] MEM_WB_MemData, MEM_WB_ALUData;
 	output reg [4:0] MEM_WB_DstReg;
-	output reg MEM_WB_MemtoReg, MEM_WB_RegWrite, MEM_WB_R_memtoReg;
+	output reg MEM_WB_JmpandLink, MEM_WB_MemtoReg, MEM_WB_RegWrite, MEM_WB_R_memtoReg;
 	
 	always @(posedge clk)
 		begin
@@ -19,6 +19,7 @@ module MEM_WB_Register (MEM_WB_R_memtoReg, MEM_WB_MemData, MEM_WB_ALUData, MEM_W
 			MEM_WB_MemtoReg <= MemtoRegIn;
 			MEM_WB_RegWrite <= RegWriteIn;
 			MEM_WB_R_memtoReg <= MEM_R_memtoReg;
+			MEM_WB_JmpandLink <= EXE_MEM_JmpandLink;
 		end
 
 endmodule
